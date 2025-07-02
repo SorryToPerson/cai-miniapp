80d0dc2333b074e78be63876b6982486a6044cbd
xulibang <xulibangyaojingjing@gmail.com>
2025-07-02 19:37:24 +0800

    feat: init

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,6 @@
-import type { UserConfigExport } from "@tarojs/cli"
-
 export default {
   mini: {},
   h5: {
-    // 确保产物为 es5
-    legacy: true,
     /**
      * WebpackChain 插件配置
      * @docs https://github.com/neutrinojs/webpack-chain
@@ -32,4 +28,4 @@ export default {
     //     }))
     // }
   }
-} satisfies UserConfigExport<'vite'>
+}
